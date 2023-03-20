e50aa779235ff7af69a87476cccdc2e28c2cf328
username <youremail@domain.com>
2023-03-20 11:05:33 -0700

    added variables

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 const canvas = document.querySelector("canvas")
 const context = canvas.getContext("2d")
+const startScreen = document.getElementById("start-screen")
+const youWonScreen = document.getElementById("you-won-screen")
+const youLostScreen = document.getElementById("you-lost-screen")
+const playAgainButton = document.querySelector("#play-again")
+const instructionsButton = document.querySelector("#play-again")
+
 
 canvas.width = innerWidth
+
