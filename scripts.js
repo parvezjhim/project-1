b2b35f28187497703c9e2de049b07bbdd26be272
username <youremail@domain.com>
2023-03-25 10:45:59 -0700

    re wrote the basics of my game

--- scripts.js
+++ scripts.js
@@ -1,270 +1,196 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-canvas.setAttribute("height", getComputedStyle(canvas).height);
-canvas.setAttribute("width", getComputedStyle(canvas).width);
+const playerShipImage = new Image()
+playerShipImage.src= "./images/spaceship.png"
 
-const startScreen = document.getElementById("start-screen");
-const youWonScreen = document.getElementById("you-won-screen");
-const youLostScreen = document.getElementById("you-lost-screen");
-const playAgainButton = document.querySelector("#play-again");
-const instructionsButton = document.querySelector("#instructions-button");
-const instructionsModal = document.querySelector("#instructions");
-const closeInstructionsButton = document.querySelector("#close-button");
-
-const openingCrawl = [
-  "17 years ago, in a galaxy real, real close...",
-  "Planet Earth has declassified Pluto as a planet...",
-  "Unbeknownst to Earth, this made the Plutonians furious",
-  "Hero: Why did I become a captain for the space force",
-  "Should have joined the damn Nav...",
-  "SUDDEN EXPLOSION",
-  "GROUND SHAKES",
-  "YOU HEAR WHAT SOUNDS LIKE THE VOICE OF GOD",
-  "ATTENTION ALL HUMANS, SEVERAL YEARS AGO YOU DECLASSIFIED OUR HOME",
-  "AS A PLANET, WE PLUTONIANS ARE HERE FOR OUR REVENGE!",
-  "LET'S SEE IF EARTH IS STILL CONSIDERED A PLANET AFTER WE ARE DONE WITH IT!",
-  "Your mission should you choose to accept it is",
-  "As the Captain of the Space Force it is your job to defend the Earth",
-  "You will be piloting the U.S.S Razor Crest which comes with a laser firing system",
-  "You will need to defeat all enemy ships before you face the big boss",
-  "Dodge their fire, and destroy their ships or all hope is lost",
-  "Godspeed, Captain",
-];
-
-const openingCrawlSpeed = 2;
-let openingCrawlY = canvas.height;
-
-function renderOpeningCrawl() {
-  ctx.fillStyle = "#000";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.font = "24px Arial";
-  ctx.fillStyle = "#fff";
-  ctx.textAlign = "center";
-  let y = openingCrawlY;
-  for (let i = 0; i < openingCrawl.length; i++) {
-    ctx.fillText(openingCrawl[i], canvas.width / 2, y);
-    y += 30;
-  }
-  openingCrawlY -= openingCrawlSpeed;
-  if (openingCrawlY < -canvas.height / 2) {
-    clearInterval(intervalId);
-    gameLoop();
-  }
-}
-
-const heroShip = {
-  x: canvas.width / 2,
-  y: canvas.height - 50,
-  width: 50,
-  height: 50,
-  speed: 5,
-  direction: null,
-  health: 3,
-};
-
-const enemyShipWidth = 50;
-const enemyShipHeight = 50;
-const enemyShipSpeed = 2;
-const enemyShipHealth = 5;
-const enemyShips = [];
-
-const bossShip = {
-  x: canvas.width / 2 - 100,
-  y: -200,
-  width: 200,
-  height: 200,
-  speed: 4,
-  direction: "down",
-  health: 50,
-};
-
-let bullets = [];
-let gameIsOver = false;
-let enemiesAreMovingDown = false;
-let frames = 0;
-let score = 0;
-let gameStarted = false;
-let intervalId = null;
-
-function startGame() {
-  gameStarted = true;
-  startScreen.style.display = "none";
-  spawnEnemyShips();
-}
-
-function spawnEnemyShips() {
-  const numCols = Math.floor(canvas.width / enemyShipWidth);
-  const numRows = 4;
-  for (let i = 0; i < numCols * numRows; i++) {
-    const x = (i % numCols) * enemyShipWidth;
-    const y = Math.floor(i / numCols) * enemyShipHeight;
-    enemyShips.push({
-      x,
-      y,
-      width: enemyShipWidth,
-      height: enemyShipHeight,
-      health: enemyShipHealth
-    });
-  }
-}
-
-function handleInput(event) {
-  if (event.key === "a") {
-    heroShip.direction = "left";
-  } else if (event.key === "d") {
-    heroShip.direction = "right";
-  } else if (event.key === " ") {
-    bullets.push({
-      x: heroShip.x + heroShip.width / 2,
-      y: heroShip.y,
-      width: 5,
-      height: 10,
-      direction: "up",
-      speed: 5,
-    });
-  }
-}
-
-function updateHeroShip() {
-  if (heroShip.direction === "left" && heroShip.x > 0) {
-    heroShip.x -= 5;
-  } else if (
-    heroShip.direction === "right" &&
-    heroShip.x + heroShip.width < canvas.width
-  ) {
-    heroShip.x += 5;
-  }
-}
-
-function updateEnemyShips() {
-  for (let i = 0; i < enemyShips.length; i++) {
-    const enemyShip = enemyShips[i];
-
-    enemyShip.x += enemyShipSpeed;
-    if (
-      enemyShip.x + enemyShipWidth > canvas.width ||
-      enemyShip.x < 0
-    ) {
-      enemiesAreMovingDown = true;
-    }
-    if (Math.random() < 0.01) {
-      bullets.push({
-        x: enemyShip.x + enemyShip.width / 2,
-        y: enemyShip.y + enemyShip.height,
-        width: 5,
-        height: 10,
-        direction: "down",
-        speed: 5,
-      });
-    }
-  }
-  if (enemiesAreMovingDown) {
-    for (let i = 0; i < enemyShips.length; i++) {
-      const enemyShip = enemyShips[i];
-      enemyShip.x += enemyShipSpeed * enemyShip.direction;
-      if (enemyShip.x <= 0 || enemyShip.x + enemyShipWidth >= canvas.width) {
-        enemyShip.direction *= -1;
+document.addEventListener("DOMContentLoaded", function() {
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 800;
+    canvas.height = 600;
+    
+    const modal = document.getElementById("instructions-modal");
+    const modalButton = document.getElementById("instructions-button");
+    const span = document.getElementsByClassName("close")[0];
+    modalButton.onclick = function (){
+      modal.style.display = "block";
+    };
+  
+    span.onclick = function (){
+      modal.style.display = "none";
+    };
+  
+    window.onclick = function (event){
+      if (event.target == modal){
+        modal.style.display = "none";
       }
-      if (frames % 60 === 0) {
-        const bullet = {
-          x: enemyShip.x + enemyShipWidth / 2,
-          y: enemyShip.y + enemyShipHeight,
-          width: 5,
-          height: 10,
-          speed: 5,
-        };
-        bullets.push(bullet);
-      }
-    }
-  }
+    };
   
-  function gameLoop() {
-    if (!gameStarted) {
-      renderStartScreen();
-      return;
-    }
-    if (gameIsOver) {
-      clearInterval(intervalId);
-      if (score === enemyShips.length * enemyShipHealth) {
-        renderYouWonScreen();
-      } else {
-        renderYouLostScreen();
-      }
-      return;
-    }
+    const openingCrawl = [
+      "17 years ago, in a galaxy real, real close...",
+      "Planet Earth has declassified Pluto as a planet...",
+      "Unbeknownst to Earth, this made the Plutonians furious",
+      "Hero: Why did I become a captain for the space force",
+      "Should have joined the damn Nav...",
+      "SUDDEN EXPLOSION",
+      "GROUND SHAKES",
+      "YOU HEAR WHAT SOUNDS LIKE THE VOICE OF GOD",
+      "ATTENTION ALL HUMANS, SEVERAL YEARS AGO YOU DECLASSIFIED OUR HOME",
+      "AS A PLANET, WE PLUTONIANS ARE HERE FOR OUR REVENGE!",
+      "LET'S SEE IF EARTH IS STILL CONSIDERED A PLANET AFTER WE ARE DONE WITH IT!",
+      "Your mission should you choose to accept it is",
+      "As the Captain of the Space Force it is your job to defend the Earth",
+      "You will be piloting the U.S.S Razor Crest which comes with a laser firing system",
+      "You will need to defeat all enemy ships before you face the big boss",
+      "Dodge their fire, and destroy their ships or all hope is lost",
+      "Godspeed, Captain",
+    ];
   
-    frames++;
+    const openingCrawlSpeed = 2;
+    let openingCrawlY = canvas.height;
   
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-    handleHeroShipMovement();
-    renderHeroShip();
-  
-    handleBullets();
-    handleEnemyShips();
-  
-    if (enemyShips.length === 0) {
-      renderBossShip();
-    } else {
-      renderEnemyShips();
-    }
-  
-    renderScore();
-  
-    if (bossShip.health <= 0) {
-      gameIsOver = true;
-      renderYouWonScreen();
-      return;
-    }
-  
-    if (heroShip.health <= 0) {
-      gameIsOver = true;
-      renderYouLostScreen();
-      return;
-    }
-  
-    requestAnimationFrame(gameLoop);
-  }
-  
-  function handleBossShip() {
-    bossShip.x += bossShip.speed * bossShip.direction;
-    if (bossShip.x <= 0 || bossShip.x + bossShip.width >= canvas.width) {
-      bossShip.direction *= -1;
-    }
-    if (frames % 60 === 0) {
-      const bullet = {
-        x: bossShip.x + bossShip.width / 2,
-        y: bossShip.y + bossShip.height,
-        width: 5,
-        height: 20,
-        speed: 5,
-        direction: 1,
-      };
-      enemyBullets.push(bullet);
-    }
-    for (let i = 0; i < enemyBullets.length; i++) {
-      const bullet = enemyBullets[i];
-      bullet.y += bullet.speed * bullet.direction;
-      if (bullet.y + bullet.height >= canvas.height) {
-        enemyBullets.splice(i, 1);
-        i--;
-      } else {
-        if (
-          bullet.x + bullet.width >= player.x &&
-          bullet.x <= player.x + player.width &&
-          bullet.y + bullet.height >= player.y &&
-          bullet.y <= player.y + player.height
-        ) {
-          gameOver();
+    function handleKeyPressEvent(e){
+      const speed = 10;
+      switch(e.key){
+        case "a":
+          playerShip.x -= speed;
           break;
-        }
-        ctx.fillStyle = "red";
-        ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+        case "d":
+          playerShip.x += speed;
+          break;
+        case " ":
+            console.log("spacebarpressed")
+          playerShip.fireBullet();
+          break;
+          
       }
+    } 
+    
+  
+    class Ship {
+        constructor(x, y, width, height, color, isAlive, speed) {
+          this.x = x;
+          this.y = y;
+          this.width = width;
+          this.height = height;
+          this.color = color;
+          this.isAlive = true;
+          this.speed = speed;
+          this.bullets = [];
+        }
+      
+        draw() {
+          ctx.fillStyle = this.color;
+          ctx.fillRect(this.x, this.y, this.width, this.height);
+          this.drawBullets();
+        }
+      
+        fireBullet() {
+          const bulletWidth = 5;
+          const bulletHeight = 10;
+          const bulletColor = "yellow";
+          const bulletSpeed = 5;
+          const bulletX = this.x + this.width / 2 - bulletWidth / 2;
+          const bulletY = this.y - bulletHeight;
+          const bullet = new Bullet(bulletX, bulletY, bulletWidth, bulletHeight, bulletColor, bulletSpeed);
+          this.bullets.push(bullet);
+          console.log("bullet", bullet)
+          console.log("bullets", this.bullets)
+        }
+      
+        drawBullets() {
+          for (let i = 0; i < this.bullets.length; i++) {
+            const bullet = this.bullets[i];
+            bullet.draw();
+            bullet.move();
+            for (let j = 0; j < alienShips.length; j++) { 
+              const alienShip = alienShips[j];
+              if (alienShip.isAlive && bullet.x < alienShip.x + alienShip.width && bullet.x + bullet.width > alienShip.x && bullet.y < alienShip.y + alienShip.height && bullet.y + bullet.height > alienShip.y) {
+                alienShip.hitCount++; 
+                if (alienShip.hitCount >= 5) {
+                  alienShip.isAlive = false; 
+                }
+                this.bullets.splice(i, 1);
+                break; 
+              }
+            }
+            if (bullet.isOffScreen()) {
+              this.bullets.splice(i, 1);
+            }
+          }
+        }
+      }
+      
+      class AlienShip {
+        constructor(x, y, width, height, color, isAlive, speed) {
+          this.x = x;
+          this.y = y;
+          this.width = width;
+          this.height = height;
+          this.color = color;
+          this.isAlive = isAlive;
+          this.speed = speed;
+          this.hitCount = 0; // change the amount of hits needed the game is too easy right now.
+        }
+      
+        draw() {
+          if (this.isAlive) {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+          }
+        }
+      
+        move() {
+          this.x += this.speed;
+      
+          if (this.x < 0 || this.x + this.width > canvas.width) {
+            this.speed = -this.speed;
+            this.y += 20;
+          }
+        }
+      }
+      
+      const alienShips = [];
+      for (let i = 0; i < 10; i++) {
+        const alienShip = new AlienShip(i * 80 + 50, 50, 30, 30, "green", true, 5);
+        alienShips.push(alienShip);
+      }
+      
+      class Bullet {
+        constructor(x, y, width, height, color, speed) {
+          this.x = x;
+          this.y = y;
+          this.width = width;
+            this.height = height;
+            this.color = color;
+            this.speed = speed;
+        }
+        draw(){
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+        move(){
+            this.y -= this.speed
+        }
+        isOffScreen(){
+            return this.y + this.height < 0;
+        }
     }
-  }
-  
-           
-  
+    document.addEventListener("keydown", handleKeyPressEvent);
+    const playerShip = new Ship(100,canvas.height - 50, 50, 50, "red", true, 10);
+    const bossShip = new Ship(200, 0, 100, 100, "purple", true, 0);
 
- 
+    
+
+    function render(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        playerShip.draw();
+        bossShip.draw();
+        for(let i =0; i< alienShips.length; i++){
+            const alienShip = alienShips[i];
+            alienShip.draw();
+            alienShip.move()
+        }
+        window.requestAnimationFrame(render);
+    }
+
+    window.requestAnimationFrame(render);
+});
